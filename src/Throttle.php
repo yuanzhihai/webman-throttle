0f4ad104619ea3d5f69562ffb1335f3f185ae144
yuanzhihai <396751927@qq.com>
2022-04-09 11:07:34 +0800

    修复 Response, Request 和 webman 中间件一致 Webman\Http\Response Webman\Http\Request

--- src/Throttle.php
+++ src/Throttle.php
@@ -13,7 +13,8 @@ namespace yzh52521\middleware;
 use Psr\SimpleCache\CacheInterface;
 use yzh52521\middleware\throttle\{CounterFixed, ThrottleAbstract};
 use Webman\Config;
-use support\{Container, Cache, Request, Response};
+use support\{Container, Cache};
+use Webman\Http\{Response,Request};
 use function sprintf;
 
 /**
