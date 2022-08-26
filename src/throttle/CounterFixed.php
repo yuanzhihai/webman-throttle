e5caf636417b9b315e7e985efc819d307dfe9ed3
yuanzhihai <396751927@qq.com>
2022-08-26 09:39:52 +0800

    解决类型转换出现的问题

--- src/throttle/CounterFixed.php
+++ src/throttle/CounterFixed.php
@@ -11,9 +11,9 @@ namespace yzh52521\middleware\throttle;
 class CounterFixed extends ThrottleAbstract
 {
 
-    public function allowRequest(string $key, float $micronow, int $max_requests, int $duration,  $cache): bool
+    public function allowRequest(string $key, float $micronow, int $max_requests, int $duration, $cache): bool
     {
-        $cur_requests       = $cache::get($key, 0);
+        $cur_requests       = (int)$cache::get($key, 0);
         $now                = (int)$micronow;
         $wait_reset_seconds = $duration - $now % $duration;     // 距离下次重置还有n秒时间
         $this->wait_seconds = $wait_reset_seconds % $duration + 1;
--- src/throttle/LeakyBucket.php
+++ src/throttle/LeakyBucket.php
@@ -11,11 +11,11 @@ namespace yzh52521\middleware\throttle;
 class LeakyBucket extends ThrottleAbstract
 {
 
-    public function allowRequest(string $key, float $micronow, int $max_requests, int $duration,  $cache): bool
+    public function allowRequest(string $key, float $micronow, int $max_requests, int $duration, $cache): bool
     {
         if ($max_requests <= 0) return false;
 
-        $last_time = $cache::get($key, 0);      // 最近一次请求
+        $last_time = (float)$cache::get($key, 0);      // 最近一次请求
         $rate      = (float)$duration / $max_requests;       // 平均 n 秒一个请求
         if ($micronow - $last_time < $rate) {
             $this->cur_requests = 1;
